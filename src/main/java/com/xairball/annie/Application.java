b7fe0a0f10dc366a4c1363bd68b880605ab30ea4
admin <admin@192.168.11.113>
2017-09-12 16:17:20 +0800

    init

--- src/main/java/com/xairball/annie/Application.java
+++ src/main/java/com/xairball/annie/Application.java
@@ -1,25 +1,49 @@
 package com.xairball.annie;
 
+import org.apache.commons.lang3.RandomUtils;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.airball.annie.mapper")
+@MapperScan(basePackages = "com.xairball.annie.mapper")
 public class Application extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
 	}
-
+	
+	
+	
 	public static void main(String[] args) throws Exception {
+		final String[][] map = new String[12][30];
 		
-		String aa= "110000　　北京市";
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(;;){
+				int x = RandomUtils.nextInt(0, map.length);
+				int y = RandomUtils.nextInt(0, map[1].length);
+				map[x][y] = "&";
+				
+				for (String[] str : map) {
+					for (int i = 0; i < str.length; i++) {
+						System.out.print(str[i]);
+					}
+					System.out.println();
+				}
+				}
+			}
+		}).start();
 		
-		SpringApplication.run(Application.class, args);
+//		String aa= "110000　　北京市";
+//		for (int i = 0; i < 10000; i++) {
+//			System.out.println(RandomUtils.nextInt(1,7));
+//		}
+		//System.out.println(DateFormatUtils.format(date, pattern)));
+//		SpringApplication.run(Application.class, args);
 //		List<String> lines = FileUtils.readLines(new File("e:/aa.txt"));
 //		StringBuilder sb = new StringBuilder();
 //		int index = 1;
